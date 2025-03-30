f61efa00ae017340682ad2806e0553bcc6fd90c4
Gimy404 <oo.cmohamedgamal71@gmail.com>
2025-03-30 19:34:34 +0200

    product feature

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRoute from "./routes/userRoute"; // Ensure this path is correct and the file exists
+import productRoute from "./routes/productRoute"; // Import product routes
+import { seedInitialProducts } from "./services/productServices";
 
 const app = express();
 const port = 3002;
@@ -11,7 +13,12 @@ mongoose
   .then(() => console.log("Mongo connected!"))
   .catch((err) => console.log("Failed to connect!", err));
 
+// seed all products to database
+seedInitialProducts()
+
 app.use('/user',userRoute)
+app.use('/product', productRoute);
+
 
 app.listen(port, () => {
   console.log(`Server is running at: http://localhost:${port}`);
